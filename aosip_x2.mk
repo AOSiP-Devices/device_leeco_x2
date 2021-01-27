b7cce2008d4bebeab7c089499c89ee098ceadcff
Juan Ramírez <juan1999l@gmail.com>
2021-01-27 12:20:33 +0000

    x2: AOSiP-ify

--- aosip_x2.mk
+++ aosip_x2.mk
@@ -21,14 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/leeco/x2/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := LeEco
 PRODUCT_DEVICE := x2
 PRODUCT_MANUFACTURER := LeEco
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := aosip_x2
 
 PRODUCT_GMS_CLIENTID_BASE := android-leeco
 
